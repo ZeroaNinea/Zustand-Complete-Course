d97fa915dc64f1c495b3a93de1b2fbe899da92d2
Catgirl <zeroaninea@gmail.com>
2026-04-10 21:28:21 +0400

    a counter implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,40 @@ import { create } from 'zustand';
 
 import './App.css';
 
-import { Button } from './components/ui/button';
+const useStore = create<{
+  count: number;
+  inc: () => void;
+  dec: () => void;
+}>((set) => ({
+  count: 0,
+  inc: () => set((state) => ({ count: state.count + 1 })),
+  dec: () => set((state) => ({ count: state.count - 1 })),
+}));
 
 function App() {
-  const useStore = create<{
-    count: number;
-    inc: () => void;
-    dec: () => void;
-  }>((set) => ({
-    count: 0,
-    inc: () => set((state) => ({ count: state.count + 1 })),
-    dec: () => set((state) => ({ count: state.count - 1 })),
-  }));
+  const store = useStore();
 
   return (
     <>
-      <Button onClick={useStore.getState().inc}>Button</Button>
+      <main>
+        <div className="border border-gray-500 w-24 h-24 flex items-center justify-center rounded-2xl text-gray-500 text-3xl font-medium">
+          {store.count}
+        </div>
+        <div>
+          <button
+            onClick={store.inc}
+            className="rounded-md p-2 m-3 bg-blue-300 hover:bg-blue-400 text-white active:bg-blue-500 cursor-pointer"
+          >
+            Increment
+          </button>
+          <button
+            onClick={store.dec}
+            className="rounded-md p-2 m-3 bg-blue-300 hover:bg-blue-400 text-white active:bg-blue-500 cursor-pointer"
+          >
+            Decrement
+          </button>
+        </div>
+      </main>
     </>
   );
 }
