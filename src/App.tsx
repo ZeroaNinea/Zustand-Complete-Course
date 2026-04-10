ebac61e194786cd0305d87bf3becf115a401c882
Catgirl <zeroaninea@gmail.com>
2026-04-10 21:06:02 +0400

    an increment button with state added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { create } from 'zustand';
+
 import './App.css';
 
 import { Button } from './components/ui/button';
 
 function App() {
+  const useStore = create<{
+    count: number;
+    inc: () => void;
+    dec: () => void;
+  }>((set) => ({
+    count: 0,
+    inc: () => set((state) => ({ count: state.count + 1 })),
+    dec: () => set((state) => ({ count: state.count - 1 })),
+  }));
+
   return (
     <>
-      <Button>Button</Button>
+      <Button onClick={useStore.getState().inc}>Button</Button>
     </>
   );
 }
